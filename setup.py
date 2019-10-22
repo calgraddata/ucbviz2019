7848fe695b2b87bfb32105791336392332e563fb
ardunn <ardunn@lbl.gov>
2019-10-21 23:29:21 -0700

    change load df to include info about the dataframe

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup, find_packages
 
-
 with open("README.md") as f:
     readme = f.read()
 
 with open("LICENSE") as f:
     license = f.read()
-
-
 
 setup(
     name="ucbviz2019",
--- ucbviz2019/data.py
+++ ucbviz2019/data.py
@@ -6,10 +6,13 @@ from ucbviz2019.constants import csvs_raw_dir
 
 
 def load_df(
-    fname, data_dir=csvs_raw_dir, remove_blank=True, drop_nan_years=True
+        fname, data_dir=csvs_raw_dir, remove_blank=True, drop_nan_years=True,
+        drop_nan_ix=True
 ):
     """
     Load a dataframe of ucb data from the refactored filename.
+
+    The name of the dataframe describes what the data is.
 
     Args:
         fname (str): The name of the file in the directory.
@@ -19,13 +22,21 @@ def load_df(
             data
         drop_nan_years (bool): If True, removes the year columns with
             all nan values (i.e., no values have been recorded/expected).
+        drop_nan_ix (bool): If True, removes rows with nan index (e.g.,
+            info-only rows).
 
     Returns:
-        (pd.DataFrame): A pandas dataframe containing the data.
+        ((pd.DataFrame, str)): A pandas dataframe containing the data and a
+            string describing what the data is.
 
     """
     test_fname_full = os.path.join(data_dir, fname)
+
+    with open(test_fname_full, "r") as f:
+        df_name = f.readlines()[0].replace(",", "'").replace("\'", "").strip()
+
     df = pd.read_csv(test_fname_full, header=1, index_col=0)
+    info = df_name
 
     if remove_blank:
         blank_key = "[blank]"
@@ -36,7 +47,10 @@ def load_df(
         na_cols = df.columns[df.isna().all()]
         df = df.drop(columns=na_cols)
 
-    return df
+    if drop_nan_ix:
+        df = df.loc[df.index.dropna()]
+
+    return df, info
 
 
 if __name__ == "__main__":
@@ -46,7 +60,10 @@ if __name__ == "__main__":
 
     for fname in os.listdir(csvs_raw_dir):
         if ".csv" in fname:
-            df = load_df(fname, csvs_raw_dir)
+            df, info = load_df(fname, csvs_raw_dir)
             print(fname)
+            # print(df.shape)
+            # print("\n")
+            print(info)
             print(df)
             print("\n\n\n")
