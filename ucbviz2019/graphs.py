306f308a66a58ca2407ede3943350deb40bdc24a
ardunn <ardunn@lbl.gov>
2019-10-22 00:04:44 -0700

    add graphs draft for lines

--- ucbviz2019/graphs.py
+++ ucbviz2019/graphs.py
@@ -3,13 +3,34 @@ import dash_core_components as dcc
 from ucbviz2019.data import load_df_and_info
 
 
-def get_total_in_state_tuition_graph_html():
-    df, info = load_df_and_info("total_in_state.csv")
+def get_generic_line_graph_html(fname):
+    df, info = load_df_and_info(fname)
 
-    df_by_program = df.T
+    # 'name': 'Trace 1',
+    # 'mode': 'markers',
+    # 'marker': {'size': 12}
 
-    print(df)
+    marker = {"size": 15}
+
+    data = []
+    df = df.T
+    for program in df.columns:
+        program_data = df[program]
+        data.append({"x": program_data.index, "y": program_data.values, "name": program, "mode": "lines+markers", "marker": marker})
+
+    layout = {
+        "clickmode": "event+select",
+        "hovermode": "x+y"
+    }
+
+    plot = dcc.Graph(
+        id="in_state_tuition_line",
+        figure={"data": data, "layout": layout}
+    )
+
+    return plot
+
 
 
 if __name__ == "__main__":
-    get_total_in_state_tuition_graph_html()
+    get_generic_line_graph_html()
--- ucbviz2019/index.py
+++ ucbviz2019/index.py
@@ -1,7 +1,12 @@
 import dash_html_components as html
 
 from ucbviz2019.app import app
+from ucbviz2019.graphs import get_generic_line_graph_html
 
 test_div = html.Div("Test layout text")
 
-app.layout = html.Div(test_div)
+
+line_graphs =
+tist = get_generic_line_graph_html()
+
+app.layout = html.Div([test_div, tist])
