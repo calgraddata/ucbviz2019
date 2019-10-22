3eed1f344831a674af57ad84bcce5fa15b6590e4
ardunn <ardunn@lbl.gov>
2019-10-21 22:04:07 -0700

    add setup and data

--- app.py
+++
@@ -1,7 +0,0 @@
-from ucbviz2019.index import app
-
-server = app.server
-
-if __name__ == '__main__':
-    print("starting...")
-    app.run_server(debug=True)
--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
-#!/usr/bin/env python
-# -*- coding: UTF-8 -*-
 from setuptools import setup, find_packages
 
-with open('README.md') as f:
+
+with open("README.md") as f:
     readme = f.read()
 
-with open('LICENSE') as f:
+with open("LICENSE") as f:
     license = f.read()
 
+
+
 setup(
-    name='ucbviz2019',
-    version='0.0.1',
-    description='Entry for UC Berkeley Graduate Data Visualization Contest',
+    name="ucbviz2019",
+    version="0.0.1",
+    description="Entry for UC Berkeley Graduate Data Visualization Contest",
     long_description=readme,
-    author='Alexander Dunn, John Dagdelen',
-    author_email='ardunn@lbl.gov, jdagdelen@lbl.gov',
-    url='https://github.com/ardunn/ucbviz2019',
+    author="Alexander Dunn, John Dagdelen",
+    author_email="denhaus@berkeley.edu, ardunn@lbl.gov, jdagdelen@lbl.gov",
+    url="https://github.com/ardunn/ucbviz2019",
     license=license,
     packages=find_packages()
 )
