6ad0018513eba88289967d0415df300ec6469cc1
ardunn <ardunn@lbl.gov>
2019-10-21 22:09:33 -0700

    update app

--- ucbviz2019/app.py
+++ ucbviz2019/app.py
@@ -5,3 +5,7 @@ A safe place for the dash app to hang out.
 """
 
 app = dash.Dash()
+app.css.config.serve_locally = True
+app.scripts.config.serve_locally = True
+app.config.suppress_callback_exceptions = True
+app.title = "UC Berkeley Graduate Data Visualization Contest"
