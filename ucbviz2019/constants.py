3f99aacee5217898a7d5535cd10b8b3402dff68d
jdagdelen <jjdagdelen@gmail.com>
2019-10-27 20:30:33 -0700

    Added program-category mappings

--- ucbviz2019/constants.py
+++ ucbviz2019/constants.py
@@ -1,10 +1,11 @@
 import os
+from pandas import read_csv
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
 assets_data_dir = os.path.join(root_dir, "assets/data")
 csvs_raw_dir = os.path.join(assets_data_dir, "v1-1_csv")
 
-programs = ['Other Programs', 'Optometry (OD)', 'Law (JD)', 'Business (MBA FT)',
+program_categories = ['Other Programs', 'Optometry (OD)', 'Law (JD)', 'Business (MBA FT)',
             'UCB-UCSF Medical (MS/MD)', 'Public Health (MPH, Dr.PH )',
             'Public Policy (MPP)', 'Engineering (M.Eng.)',
             'CED (M.Arch., MCP., MLA, and MUD)', 'Social Welfare (MSW)',
@@ -14,3 +15,12 @@ programs = ['Other Programs', 'Optometry (OD)', 'Law (JD)', 'Business (MBA FT)',
             'Journalism (MJ)', 'CEE (MS)']
 
 this_year = "2019"
+
+df = read_csv("ucbviz2019/assets/data/UCB Programs.csv")
+programs = list(df['Graduate Programs'])
+degrees = list(df['Degrees'])
+categories = list(df['Category Key'])
+
+program_category_mappings = {}
+for i in range(len(programs)):
+    program_category_mappings["{} ({})".format(programs[i], degrees[i])] = categories[i]
--- ucbviz2019/graphs/fees_stacked_bar.py
+++ ucbviz2019/graphs/fees_stacked_bar.py
@@ -1,6 +1,6 @@
 from ucbviz2019.data import load_df_and_info
 import plotly.graph_objects as go
-from ucbviz2019.constants import programs
+from ucbviz2019.constants import program_category_mappings, program_categories
 
 fees = ['registration_student_services_fee.csv',
         'health_insurance_fee.csv',
@@ -14,25 +14,39 @@ fee_labels = {'health_insurance_fee.csv': 'Health Insurance Fee',
               'other_misc_fees.csv': 'Other Misc. Fees'}
 
 
-def generate_fee_stack_plot(program):
+def generate_fee_stack_plot(program="Other Programs"):
     """
     Generates a stacked bar chart for the specified program.
 
     Args:
-        program: (str) Name of program
+        program: (str) Name of program or program category
 
     Returns:
         plotly graph object.
 
     """
-    if program not in programs:
-        raise ValueError("Program must be one of {}".format(programs))
 
+    if program in program_category_mappings:
+        program = program_category_mappings[program]
     data = []
     for fee in fees:
         df, dsc = load_df_and_info(fee)
-        data.append(go.Bar(name=fee_labels[fee], x=df.loc[program].keys(), y=df.loc[program]))
+        data.append(go.Bar(name=fee_labels[fee], x=df.loc[program].keys(), y=df.loc[program],
+                           hovertemplate="%{x}: $%{y}/semester "))
     fig = go.Figure(data=data)
+    fig.update_layout(
+        title=go.layout.Title(
+            text="Fees for {}".format(program),
+            x=0.4,
+            y=0.87),
+        xaxis=dict(
+            title=go.layout.xaxis.Title(text="Academic Year"),
+            tickmode='array',
+            tickvals=df.loc[program].keys(),
+            ticktext=["{}-{}".format(year, str(int(year) + 1)) for year in df.loc[program].keys()]),
+        yaxis=dict(
+            title=go.layout.yaxis.Title(text="Total ($)")
+        ))
     fig.update_layout(barmode='stack')
     return fig
 
