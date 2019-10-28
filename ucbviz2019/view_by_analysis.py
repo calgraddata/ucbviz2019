822c9e3ec37a6e3674ed4f73ce57c5859a0cdbbb
ardunn <ardunn@lbl.gov>
2019-10-27 20:41:03 -0700

    update

--- ucbviz2019/view_by_analysis.py
+++ ucbviz2019/view_by_analysis.py
@@ -2,4 +2,14 @@ import dash_html_components as html
 
 def app_view_html():
 
-    return html.Div("Hi there, this is the by analysis page")
+    return html.Div("Hi there, this is the by analysis page")1
+
+
+
+def fee_increase_html():
+    """
+    Display the fee increase as a stacked bar chart.
+    Returns:
+
+    """
+    pass
