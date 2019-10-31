2b4a6ccb4db9638e1cf3b5306f609ecfab6e9064
ardunn <ardunn@lbl.gov>
2019-10-30 23:46:50 -0700

    unscrew the layout

--- ucbviz2019/view_by_degree.py
+++ ucbviz2019/view_by_degree.py
@@ -7,13 +7,13 @@ def app_view_html():
     return [html.Div([
         html.P("Explore Cost of Attendance by Program",
                className="is-size-3"),
-        html.Div(id="degree_card_container"),
         # html.Div(style={'padding': 10}),
         html.Label("Find your degree program:"),
         dcc.Dropdown(options=get_program_options(),
                      id='degree_program_dropdown',
                      value="Law (J.D., LL.M., J.S.D.)",
                      placeholder='Start typing program name or degree type (e.g. Ph.D, M.Eng., ...)'),
+        html.Div(id="degree_card_container"),
         html.Div(style={'padding': 10}),
         html.Div(id="degree_tuition_plot", children=[]),
         html.Div(id="degree_fees_plot", children=[])
